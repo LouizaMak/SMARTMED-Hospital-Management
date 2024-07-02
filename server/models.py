a646666be3259a3e3a28b225e5d18072ceda86dc
Louiza Mak <louiza.mak@gmail.com>
2024-07-02 14:06:15 -0500

    add doctor and appointment models and db tables

--- server/models.py
+++ server/models.py
@@ -11,3 +11,37 @@ class Patient(db.Model, SerializerMixin):
     last_name = db.Column(db.String, nullable = False)
     age = db.Column(db.Integer, nullable = False)
     gender = db.Column(db.String, nullable = False)
+
+    appointments = db.relationship('Appointment', back_populates='patient', cascade='all, delete-orphan')
+
+    serialize_rules = ('-appointments.patient',)
+
+class Appointment(db.Model, SerializerMixin):
+    __tablename__ = 'appointments'
+
+    id = db.Column(db.Integer, primary_key = True)
+    date = db.Column(db.String, nullable = False)
+    hour = db.Column(db.Integer, nullable = False)
+    patient_id = db.Column(db.Integer, db.ForeignKey('patients.id'))
+    doctor_id = db.Column(db.Integer, db.ForeignKey('doctors.id'))
+
+    patient = db.relationship('Patient', back_populates='appointments')
+    doctor = db.relationship('Doctor', back_populates='appointments')
+
+    serialize_rules = ('-patient.appointments', 'doctor.appointments')
+
+class Doctor(db.Model, SerializerMixin):
+    __tablename__ = 'doctors'
+
+    id = db.Column(db.Integer, primary_key = True)
+    npi = db.Column(db.Integer, nullable = False)
+    first_name = db.Column(db.String, nullable = False)
+    last_name = db.Column(db.String, nullable = False)
+    gender = db.Column(db.String, nullable = False)
+    field = db.Column(db.String, nullable = False)
+
+    appointments = db.relationship('Appointment', back_populates='doctor', cascade='all, delete-orphan')
+
+    serialize_rules = ('-appointments.doctor',)
+
+
--- server/seed.py
+++ server/seed.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 # Standard library imports
-from random import randint, choice as rc
+from random import randint, choice
+import string
 
 # Remote library imports
 from faker import Faker
 
 # Local imports
 from app import app
-from models import db, Patient
+from models import db, Patient, Doctor, Appointment
 
 def create_patients():
     patients = []
@@ -22,16 +23,71 @@ def create_patients():
         patients.append(p)
     return patients
 
+def generate_npi():
+    npi_array = ["1"]
+    for i in range(1,10):
+        npi_array.append(str(randint(0,9)))
+    npi = ''.join(npi_array)
+    return npi
+
+def generate_taxonomy():
+    array = []
+    for i in range(1,5):
+        array.append(str(randint(1,9)))
+    for i in range(1,3):
+        array.append(choice(string.ascii_uppercase))
+    for i in range(1, 4):
+        array.append(str(randint(1,9)))
+    array.append("X")
+    return ''.join(array)
+    
+def create_doctors():
+    doctors = []
+    for _ in range(20):
+        d = Doctor(
+            npi = generate_npi(),
+            first_name = fake.first_name(),
+            last_name = fake.last_name(),
+            gender = fake.random_element(elements=('F', 'M')),
+            field = generate_taxonomy()
+        )
+        doctors.append(d)
+    return doctors
+
+def create_appointments(patients, doctors):
+    appointments = []
+    for _ in range(20):
+        a = Appointment(
+            date = fake.date(),
+            hour = fake.random_int(min = 8, max = 17),
+            patient_id = choice(patients).id,
+            doctor_id = choice(doctors).id
+        )
+        appointments.append(a)
+    return appointments
+        
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
         print("Clearing db...")
         Patient.query.delete()
+        Doctor.query.delete()
+        Appointment.query.delete()
 
         print("Seeding patients...")
         patients = create_patients()
         db.session.add_all(patients)
+        db.session.commit()
+    
+        print("Seeding doctors...")
+        doctors = create_doctors()
+        db.session.add_all(doctors)
+        db.session.commit()
+
+        print("Seeding appointments...")
+        appointments = create_appointments(patients, doctors)
+        db.session.add_all(appointments)
         db.session.commit()
 
         print("Done seeding!")

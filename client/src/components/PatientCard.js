2acf5966cf6bd7bb1a9728e50dad6d71b7cf8903
Louiza Mak <louiza.mak@gmail.com>
2024-07-18 11:43:25 -0500

    update README and change tab logo

--- client/src/components/PatientCard.js
+++ client/src/components/PatientCard.js
@@ -5,10 +5,16 @@ function PatientCard({patient}) {
     return(
         <div className="card">
             <h3>{patient.last_name}, {patient.first_name} </h3>
-            <p>Birthday: {patient.birthday} [{patient.age}]</p>
-            <p>Biological Gender: {patient.gender}</p>
-            <p>Phone Number: {patient.phone}</p>
-            <p>Address: {patient.address}</p>
+            <div className="patient-content">
+                <div>
+                    <p>Birthday: {patient.birthday} [{patient.age}]</p>
+                    <p>Biological Gender: {patient.gender}</p>
+                </div>
+                <div>
+                    <p>Phone Number: {patient.phone}</p>
+                    <p>Address: {patient.address}</p>
+                </div>
+            </div>
         </div>
     )
 }

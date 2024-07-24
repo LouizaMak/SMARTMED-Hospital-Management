b276f3aa606ada5a4a727550e130be69f297b5e2
Louiza Mak <louiza.mak@gmail.com>
2024-07-23 20:50:56 -0500

    remove unused association proxy import

--- server/models.py
+++ server/models.py
@@ -1,5 +1,4 @@
 from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import validates
 from datetime import datetime, date
 
